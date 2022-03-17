c5f916c6a2d3f2426723011510203cc975680482
Subhasree Mohan <subhasreemohan07@gmail.com>
2022-03-17 09:45:27 +0000

    Added new function to validate principal value

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+    var it = "If you deposite "+ principal + "\n at the interest of " +rate+"\n You will receive an amount " +interest+ "\n at the end of the year "+year;
+    document.getElementById("result").innerText= it;
 }
-
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
+
+function numchk()
+{
+    var num = document.getElementById("principal").value;
+    if(num <=0){
+     alert("Enter positive number");
+    }
+    
+    document.getElementById("principal").focus();
+}
+
 
         
